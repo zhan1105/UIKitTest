bd2413a3f88da121620a5a924a734c7ca27d6b46
Johnny <zhan0619@gmail.com>
2024-08-27 12:10:12 +0800

    文字格式判斷系統

--- UIKitTest/文字格式辨識/TextSettingScreen.swift
+++ UIKitTest/文字格式辨識/TextSettingScreen.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
-class TextSettingScreen: UIViewController, UITextFieldDelegate {
+class TextSettingScreen: UIViewController {
     
-    var selectedText = String()
-        
+    let myTitleBar = MyTitleBar()
+    let subScreen = TextSettingUI()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .groupBg
         setupUI()
+    }
+    
+    func checkTextFormat(){
+        
+        if let selectedText = subScreen.firstTextField.text {
+            if selectedText.isEmpty {
+                let message = subScreen.firstTextField.placeholder ?? "輸入內容不可為空"
+                showMessage(title: "錯誤提醒", message: message)
+            } else {
+                // 使用正則表達式進行檢查
+                let regex = "^[A-Z][A-Z0-9][0-9]{8}$"
+                let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+                
+                if predicate.evaluate(with: selectedText) {
+                    showMessage(title: "成功訊息", message: "")
+                } else {
+                    // selectedText 不符合正則表達式
+                    showMessage(title: "格式錯誤", message: "輸入內容的格式不正確")
+                }
+            }
+        }
     }
 }
 extension TextSettingScreen {
@@ -53,8 +75,27 @@ extension TextSettingScreen {
             subScreen.trailingAnchor.constraint(equalTo: appScreen.trailingAnchor),
         ])
     }
-    
-    
 }
 
-
+extension TextSettingScreen: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        
+        // 計算更新後的字串長度
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        if textField == subScreen.firstTextField || textField == subScreen.thirdTextField {
+            // 限制總字數為10
+            return updatedText.count <= 10
+        } else if textField == subScreen.secondTextField {
+            // 限制只能輸入大寫英文
+            let allowedCharacters = CharacterSet.uppercaseLetters
+            let characterSet = CharacterSet(charactersIn: string)
+            return allowedCharacters.isSuperset(of: characterSet)
+        }
+        
+        return true
+    }
+}
--- UIKitTest/文字格式辨識/UI/TextSettingUI.swift
+++ UIKitTest/文字格式辨識/UI/TextSettingUI.swift
@@ -10,8 +10,12 @@ import UIKit
 class TextSettingUI: UIView {
     
     var firstTextField = UITextField()
+    var secondTextField = UITextField()
+    var thirdTextField = UITextField()
     var checkButton = UIButton(type: .system)
     var spacer = UIView()
+    
+    var buttonAction: (() -> Void)?
     
     init() {
         super.init(frame: .zero)
@@ -24,26 +28,25 @@ class TextSettingUI: UIView {
     
     private func setupUI() {
         
-        firstTextField.borderStyle = .none
-        firstTextField.placeholder = "請輸入1~2位大寫英文+8~9位數字"
-        firstTextField.textColor = .black
-        firstTextField.font = UIFont.title3
-        firstTextField.backgroundColor = .white
-        firstTextField.translatesAutoresizingMaskIntoConstraints = false
+        firstTextField = creatTextField(placeholder: "請輸入1~2位大寫英文+8~9位數字")
+        secondTextField = creatTextField(placeholder: "請輸入大寫英文")
+        thirdTextField = creatTextField(placeholder: "請輸入1位大寫英文+9位數字")
         
         checkButton.setTitle("檢查格式", for: .normal)
         checkButton.titleLabel?.font = UIFont.systemFont(ofSize: UIFont.title2.pointSize, weight: .bold)
         checkButton.tintColor = .white
+        checkButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
         checkButton.backgroundColor = .blue
         checkButton.layer.cornerRadius = 10
         checkButton.translatesAutoresizingMaskIntoConstraints = false
-                
+        
         spacer.backgroundColor = .clear
         spacer.translatesAutoresizingMaskIntoConstraints = false
         
-        let subScreen = UIStackView(arrangedSubviews: [firstTextField, checkButton, spacer])
+        let subScreen = UIStackView(arrangedSubviews: [firstTextField, secondTextField, thirdTextField, checkButton, spacer])
         subScreen.axis = .vertical
         subScreen.distribution = .fill
+        subScreen.alignment = .center
         subScreen.spacing = 15
         
         addSubview(subScreen)
@@ -57,14 +60,43 @@ class TextSettingUI: UIView {
             subScreen.bottomAnchor.constraint(equalTo: bottomAnchor),
             
             firstTextField.heightAnchor.constraint(equalTo: subScreen.heightAnchor, multiplier: 0.1),
+            secondTextField.heightAnchor.constraint(equalTo: subScreen.heightAnchor, multiplier: 0.1),
+            thirdTextField.heightAnchor.constraint(equalTo: subScreen.heightAnchor, multiplier: 0.1),
             checkButton.heightAnchor.constraint(equalTo: subScreen.heightAnchor, multiplier: 0.1),
-            spacer.heightAnchor.constraint(equalTo: subScreen.heightAnchor, multiplier: 0.8),
+            spacer.heightAnchor.constraint(equalTo: subScreen.heightAnchor, multiplier: 0.6),
             
             firstTextField.leadingAnchor.constraint(equalTo: subScreen.leadingAnchor, constant: 10),
             firstTextField.trailingAnchor.constraint(equalTo: subScreen.trailingAnchor, constant: -10),
             
+            secondTextField.leadingAnchor.constraint(equalTo: subScreen.leadingAnchor, constant: 10),
+            secondTextField.trailingAnchor.constraint(equalTo: subScreen.trailingAnchor, constant: -10),
+            
+            thirdTextField.leadingAnchor.constraint(equalTo: subScreen.leadingAnchor, constant: 10),
+            thirdTextField.trailingAnchor.constraint(equalTo: subScreen.trailingAnchor, constant: -10),
+            
             checkButton.leadingAnchor.constraint(equalTo: subScreen.leadingAnchor, constant: 10),
-            checkButton.trailingAnchor.constraint(equalTo: subScreen.trailingAnchor, constant: -10),
+            checkButton.trailingAnchor.constraint(equalTo: subScreen.trailingAnchor, constant: -10)
         ])
+    }
+    
+    func creatTextField(placeholder: String) -> UITextField {
+        
+        let newTextField = UITextField()
+        newTextField.borderStyle = .none
+        newTextField.layer.cornerRadius = 10
+        newTextField.placeholder = placeholder
+        newTextField.textColor = .black
+        newTextField.font = UIFont.title3
+        newTextField.textAlignment = .center
+        newTextField.backgroundColor = .white
+        newTextField.keyboardType = .asciiCapable
+        newTextField.autocapitalizationType = .allCharacters
+        newTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return newTextField
+    }
+    
+    @objc private func backButtonTapped() {
+        buttonAction?()
     }
 }
